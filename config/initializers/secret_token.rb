7c8b4048ee2945c8ab454ade60c4f7f31cb0e9d7
Rainux Luo <rainux@gmail.com>
2013-04-22 15:09:24 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EchidnaUi::Application.config.secret_token = '51b3974e3a39dad79307cca2755b109333e32a408b08c03087c9964c46a9125888388c1280c3334e06650836a93577ca3864f738bcae68d35ae13b7809a1230c'

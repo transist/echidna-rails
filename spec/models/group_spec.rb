687ec282553a67536a39426b60a5c95163d8a267
Richard Huang <flyerhzm@gmail.com>
2013-05-08 16:53:27 +0800

    fix typo, validate_inclusions_of => validate_inclusion_of

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Group do
-  it { should validate_inclusions_of(:gender).to_allow(%w(Male Female Both)) }
+  it { should validate_inclusion_of(:gender).to_allow(%w(Male Female Both)) }
   it { should have_many :persons }
   it { should have_many :cities }
 end

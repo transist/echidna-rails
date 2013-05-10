e78973fd54f8030412276d27360ed33348336503
Richard Huang <flyerhzm@gmail.com>
2013-05-10 23:41:08 +0800

    make daily and hourly top trends test results look better

--- spec/models/daily_stat_spec.rb
+++ spec/models/daily_stat_spec.rb
@@ -14,7 +14,11 @@ describe DailyStat do
     end
 
     it "returns word1, word3 and word2 for group 1" do
-      expect(DailyStat.top_trends(1)).to eq [{word: "word2", z_score: 1.4804519606800843}, {word: "word1", z_score: 1.4804519606800841}, {word: "word3", z_score: 1.4804519606800841}]
+      expect(DailyStat.top_trends(1)).to eq [
+        {word: "word2", z_score: 1.4804519606800843},
+        {word: "word1", z_score: 1.4804519606800841},
+        {word: "word3", z_score: 1.4804519606800841}
+      ]
     end
 
     it "returns nothing for group 2" do
@@ -22,11 +26,19 @@ describe DailyStat do
     end
 
     it "checks history for only 1 hour" do
-      expect(DailyStat.top_trends(1, days: 1)).to eq [{word: "word1", z_score: 0}, {word: "word2", z_score: 0}, {word: "word3", z_score: 0}]
+      expect(DailyStat.top_trends(1, days: 1)).to eq [
+        {word: "word1", z_score: 0},
+        {word: "word2", z_score: 0},
+        {word: "word3", z_score: 0}
+      ]
     end
 
     it "query 2 days data" do
-      expect(DailyStat.top_trends(1, days: 20)).to eq [{word: "word1", z_score: 1.3851386144545532}, {word: "word3", z_score: 1.356305321707554}, {word: "word2", z_score: 1.1815597860975298}]
+      expect(DailyStat.top_trends(1, days: 20)).to eq [
+        {word: "word1", z_score: 1.3851386144545532},
+        {word: "word3", z_score: 1.356305321707554},
+        {word: "word2", z_score: 1.1815597860975298}
+      ]
     end
   end
 end
--- spec/models/hourly_stat_spec.rb
+++ spec/models/hourly_stat_spec.rb
@@ -14,7 +14,11 @@ describe HourlyStat do
     end
 
     it "returns word1, word3 and word2 for group 1" do
-      expect(HourlyStat.top_trends(1)).to eq [{word: "word2", z_score: 1.4804519606800843}, {word: "word1", z_score: 1.4804519606800841}, {word: "word3", z_score: 1.4804519606800841}]
+      expect(HourlyStat.top_trends(1)).to eq [
+        {word: "word2", z_score: 1.4804519606800843},
+        {word: "word1", z_score: 1.4804519606800841},
+        {word: "word3", z_score: 1.4804519606800841}
+      ]
     end
 
     it "returns nothing for group 2" do
@@ -22,11 +26,19 @@ describe HourlyStat do
     end
 
     it "checks history for only 1 hour" do
-      expect(HourlyStat.top_trends(1, hours: 1)).to eq [{word: "word1", z_score: 0}, {word: "word2", z_score: 0}, {word: "word3", z_score: 0}]
+      expect(HourlyStat.top_trends(1, hours: 1)).to eq [
+        {word: "word1", z_score: 0},
+        {word: "word2", z_score: 0},
+        {word: "word3", z_score: 0}
+      ]
     end
 
     it "query 2 days data" do
-      expect(HourlyStat.top_trends(1, hours: 20)).to eq [{word: "word1", z_score: 1.429954466009161}, {word: "word3", z_score: 1.4226184910682367}, {word: "word2", z_score: 1.3661642830588334}]
+      expect(HourlyStat.top_trends(1, hours: 20)).to eq [
+        {word: "word1", z_score: 1.429954466009161},
+        {word: "word3", z_score: 1.4226184910682367},
+        {word: "word2", z_score: 1.3661642830588334}
+      ]
     end
   end
 end

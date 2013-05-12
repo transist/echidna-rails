59a8326f337465fcf54d74e7f2767c121d7018c4
Richard Huang <flyerhzm@gmail.com>
2013-05-12 16:39:49 +0800

    allow to select multiple age ranges

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -1,17 +1,20 @@
 class Panel
   include Mongoid::Document
 
+  field :age_ranges, type: Array
+  field :gender
+
   belongs_to :user
   embeds_many :cities
-  field :age_range, type: Array
-  field :gender
+
+  before_save :remove_empty_values
 
   def city_ids=(city_ids)
     self.cities = city_ids.map { |city_id|
       if city_id.present?
         City.find(city_id)
       end
-    }.compact
+    }
   end
 
   def city_ids
@@ -19,6 +22,13 @@ class Panel
   end
 
   def to_s
-    '%s %s %s' % [age_range, gender, cities.map(&:name)]
+    '%s %s %s' % [age_ranges.join(', '), gender, cities.map(&:name)]
+  end
+
+  private
+
+  def remove_empty_values
+    self.cities.reject! { |city_id| city_id.blank? }
+    self.age_ranges.reject! { |city_id| city_id.blank? }
   end
 end

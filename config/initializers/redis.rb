cd9423ae1e8fc61c95ddaf4e08e5897400c7534e
Rainux Luo <rainux@gmail.com>
2013-05-15 00:19:21 +0800

    Move redis config to application.yml and simplify namespace

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,5 @@
-ENV['ECHIDNA_REDIS_HOST'] ||= '127.0.0.1'
-ENV['ECHIDNA_REDIS_PORT'] ||= '6379'
-ENV['ECHIDNA_REDIS_NAMESPACE'] = "e:#{ENV['USER']}:#{Rails.env[0]}"
-
 $redis = Redis::Namespace.new(
-  ENV['ECHIDNA_REDIS_NAMESPACE'],
+  "e:#{Rails.env[0]}",
   redis: Redis.new(
     host: ENV['ECHIDNA_REDIS_HOST'],
     port: ENV['ECHIDNA_REDIS_PORT'],

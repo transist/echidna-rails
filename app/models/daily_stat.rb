aeb3d9c389339911a5e3aecb657cb8a5c03d3303
Richard Huang <flyerhzm@gmail.com>
2013-05-16 00:02:06 +0800

    add words_stats and aggregate_stats to DailyStat

--- app/models/daily_stat.rb
+++ app/models/daily_stat.rb
@@ -5,34 +5,56 @@ class DailyStat
   field :date, type: Date
   field :stats, type: Array # [{day: 0, count: 1}, {day: 1, count: 2}, {day: 10, count: 0}]
 
+  scope :group_stats_in_period, ->(group_id, current_time, start_time) {
+    where(group_id: group_id).
+    lte(date: current_time.to_date.beginning_of_month).
+    gte(date: start_time.to_date.beginning_of_month).
+    asc(:date)
+  }
   belongs_to :group
 
-  def self.top_trends(panel, options={})
-    current_time = Time.now.beginning_of_day
-    days = options[:days] || 7
-    start_time = current_time.ago(days.days)
+  def self.aggregate_stats(history_stats, current_stats)
+    h_stats = history_stats.inject({}) do |h_stats, single_stats|
+      single_stats.each do |word, stats|
+        h_stats[word] ||= []
+        h_stats[word] << stats
+      end
+      h_stats
+    end
+    c_stats = current_stats.inject({}) do |c_stats, single_stats|
+      single_stats.each do |word, stats|
+        c_stats[word] ||= 0
+        c_stats[word] += stats
+      end
+      c_stats
+    end
+    c_stats.map { |word, c_stat|
+      {word: word, z_score: FAZScore.new(0.5, h_stats[word].transpose.map { |stats| stats.reduce(&:+) }).score(c_stat)}
+    }.sort_by { |stat| -stat[:z_score] }
+  end
+
+  def self.words_stats(group_id, options={})
+    current_time = options[:current_time].beginning_of_day
+    start_time = options[:start_time].beginning_of_day
+    current_time_distance = (current_time - start_time) / 1.day.to_i
 
     history_stats = {}
     current_stats = {}
-    panel.groups.each do |group|
-      self.where(group_id: group.id).lte(date: current_time.to_date.beginning_of_month).gte(date: start_time.to_date.beginning_of_month).asc(:date).each do |daily_stat|
-        word = daily_stat.word
-        time = daily_stat.date.to_time
-        daily_stat.stats.each do |stat|
-          time = time.change(day: stat["day"])
-          stat_count = stat["count"]
-          if time >= start_time && time < current_time
-            history_stats[word] ||= Array.new((current_time - start_time) / 1.day.to_i, 0)
-            history_stats[word][(time - start_time) / 1.day.to_i] += stat_count
-          elsif time == current_time
-            current_stats[word] ||= 0
-            current_stats[word] += stat_count
-          end
+    self.group_stats_in_period(group_id, current_time, start_time).each do |daily_stat|
+      word = daily_stat.word
+      time = daily_stat.date.to_time
+      daily_stat.stats.each do |stat|
+        time = time.change(day: stat["day"])
+        time_distance = (time - start_time) / 1.day.to_i
+        stat_count = stat["count"]
+        if time >= start_time && time < current_time
+          history_stats[word] ||= Array.new(current_time_distance, 0)
+          history_stats[word][time_distance] = stat_count
+        elsif time == current_time
+          current_stats[word] = stat_count
         end
       end
     end
-    current_stats.map { |word, current_stat|
-      {word: word, z_score: FAZScore.new(0.5, history_stats[word]).score(current_stat)}
-    }.sort_by { |stat| -stat[:z_score] }
+    {history_stats: history_stats, current_stats: current_stats}
   end
 end
--- spec/models/daily_stat_spec.rb
+++ spec/models/daily_stat_spec.rb
@@ -3,52 +3,113 @@ require 'spec_helper'
 describe DailyStat do
   it { should belong_to :group }
 
-  describe ".top_trends" do
-    let(:group1) { create :group }
-    let(:group2) { create :group }
-    let(:panel) {
-      panel = create(:panel).tap do |panel|
-        panel.groups << group1
-        panel.groups << group2
-      end
-    }
-    let(:other_panel) { create(:panel) }
-
-    before do
-      Timecop.freeze(Time.now.change(day: 10))
-      prepare_daily_stats(panel)
+  let(:group1) { create :group }
+  let(:group2) { create :group }
+  let(:panel) {
+    panel = create(:panel).tap do |panel|
+      panel.groups << group1
+      panel.groups << group2
     end
+  }
+  let(:other_panel) { create(:panel) }
 
-    after do
-      Timecop.return
-    end
+  before do
+    Timecop.freeze(Time.now.change(day: 10))
+    prepare_daily_stats(panel)
+  end
 
-    it "returns 中国, 日本 and 美国" do
-      expect(DailyStat.top_trends(panel)).to eq [
+  after do
+    Timecop.return
+  end
+
+  describe ".aggregate_stats" do
+    it "returns words with z_scores" do
+      expect(DailyStat.aggregate_stats(
+        [{
+          "中国" => [30, 40, 50, 60, 70, 80, 90],
+          "美国" => [3, 4, 5, 6, 7, 8, 9],
+          "日本" => [36, 48, 60, 72, 84, 96, 108]
+        }],
+        [{
+          "中国" => 100, "美国" => 10, "日本" => 120
+        }]
+      )).to eq([
         {word: "美国", z_score: 1.4804519606800843},
         {word: "中国", z_score: 1.4804519606800841},
         {word: "日本", z_score: 1.4804519606800841}
-      ]
+      ])
     end
 
     it "returns nothing for other panel" do
-      expect(DailyStat.top_trends(other_panel)).to be_empty
-    end
-
-    it "checks history for only 1 hour" do
-      expect(DailyStat.top_trends(panel, days: 1)).to eq [
+      expect(DailyStat.aggregate_stats(
+        [{
+          "中国" => [90], "美国" => [9], "日本" => [108]
+        }],
+        [{
+          "中国" => 100, "美国" => 10, "日本" => 120
+        }]
+      )).to eq([
         {word: "中国", z_score: 0},
         {word: "美国", z_score: 0},
         {word: "日本", z_score: 0}
-      ]
+      ])
     end
 
-    it "query 2 days data" do
-      expect(DailyStat.top_trends(panel, days: 20)).to eq [
-        {word: "中国", z_score: 1.3851386144545532},
-        {word: "日本", z_score: 1.356305321707554},
-        {word: "美国", z_score: 1.1815597860975298}
-      ]
+    it "aggregates multiple stats" do
+      expect(DailyStat.aggregate_stats(
+        [{
+          "中国" => [30, 40, 50, 60, 70, 80, 90],
+          "美国" => [3, 4, 5, 6, 7, 8, 9],
+          "日本" => [36, 48, 60, 72, 84, 96, 108]
+        },
+        {
+          "中国" => [30, 40, 50, 60, 70, 80, 90],
+          "美国" => [3, 4, 5, 6, 7, 8, 9],
+          "日本" => [36, 48, 60, 72, 84, 96, 108]
+        }],
+        [{
+          "中国" => 100, "美国" => 10, "日本" => 120
+        }, {
+          "中国" => 100, "美国" => 10, "日本" => 120
+        }]
+      )).to eq([
+        {word: "美国", z_score: 1.4804519606800843},
+        {word: "中国", z_score: 1.4804519606800841},
+        {word: "日本", z_score: 1.4804519606800841}
+      ])
+    end
+  end
+
+  describe ".word_stats" do
+    it "returns 中国, 日本 and 美国 stats" do
+      stats = DailyStat.words_stats(group1.id, current_time: Time.now, start_time: 7.days.ago)
+      expect(stats).to eq({
+        history_stats: {
+          "中国" => [30, 40, 50, 60, 70, 80, 90],
+          "美国" => [3, 4, 5, 6, 7, 8, 9],
+          "日本" => [36, 48, 60, 72, 84, 96, 108]
+        },
+        current_stats: {
+          "中国" => 100, "美国" => 10, "日本" => 120
+        }
+      })
+    end
+
+    it "returns nothing for non existent group" do
+      stats = DailyStat.words_stats(-1, current_time: Time.now, start_time: 7.days.ago)
+      expect(stats).to eq({history_stats: {}, current_stats: {}})
+    end
+
+    it "returns stats for only 1 day" do
+      stats = DailyStat.words_stats(group1.id, current_time: Time.now, start_time: 1.day.ago)
+      expect(stats).to eq({
+        history_stats: {
+          "中国" => [90], "美国" => [9], "日本" => [108]
+        },
+        current_stats: {
+          "中国" => 100, "美国" => 10, "日本" => 120
+        }
+      })
     end
   end
 end

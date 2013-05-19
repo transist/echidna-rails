159f4ca1ffe10fb50b8234272698d9f5dae64491
Rainux Luo <rainux@gmail.com>
2013-05-20 03:37:06 +0800

    Configure redis namespace for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,4 +3,10 @@ Sidekiq.configure_server do |config|
     chain.add Kiqstand::Middleware
     chain.remove Sidekiq::Middleware::Server::ActiveRecord
   end
+
+  config.redis = { namespace: "e:sidekiq:#{Rails.env[0]}" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { namespace: "e:sidekiq:#{Rails.env[0]}" }
 end

b75c444d54bfc2b5cd9f7a2f7e121929bfb88a89
Richard Huang <flyerhzm@gmail.com>
2013-05-20 20:09:14 +0800

    fix undefined target_id

--- app/workers/tweet_worker.rb
+++ app/workers/tweet_worker.rb
@@ -3,15 +3,16 @@ class TweetWorker
   sidekiq_options :queue => :spider
 
   def perform(tweet_attrs)
+    target_id = tweet_attrs.delete('target_person_id')
     person = Person.where(
       target_source: tweet_attrs.delete('target_source'),
-      target_id: tweet_attrs.delete('target_person_id')
+      target_id: target_id
     ).first
-    
+
     if person.nil?
       TencentAgent.all.first.sample_user(target_id, query_type: :fopenid)
     end
-    
+
     person.tweets.create(tweet_attrs)
   end
 end

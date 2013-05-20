7030c0c873cba3cfc31588f6d75b4d44c0099577
simsicon <simsicon@gmail.com>
2013-05-20 15:36:09 +0800

    Fix a typo in last commit

--- app/workers/tracking_person_worker.rb
+++ app/workers/tracking_person_worker.rb
@@ -5,7 +5,7 @@ class TrackingPersonWorker
     person = Person.find person_id
 
     agent = TencentAgent.get_agent_with_capacity
-    agent.track_users([person.targe_id])
+    agent.track_users([person.target_id])
   end
 
 end

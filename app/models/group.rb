aef61f644b2eb664f1aac14fca958727204bf7f5
Rainux Luo <rainux@gmail.com>
2013-05-22 02:36:28 +0800

    Remove obsolete code

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,6 @@
 class Group
   include Mongoid::Document
 
-  GET_GROUP_ID_URL = 'http://echidna.transi.st:62300/group_id'
-  Z_SCORES_URL = 'http://echidna.transi.st:62300/z-scores'
-
   field :start_birth_year, type: Integer
   field :end_birth_year, type: Integer
   field :gender, type: String
@@ -35,22 +32,6 @@ class Group
 
   def add_person(person)
     people << person
-  end
-
-  def tier
-    Tier.find(tier_id)
-  end
-
-  def get_group_id
-    @group_id ||= begin
-                    response = Faraday.get(
-                      GET_GROUP_ID_URL,
-                      tier_id: tier_id,
-                      age_range: age_range,
-                      gender: gender
-                    )
-                    MultiJson.load(response.body)['id']
-                  end
   end
 
   def z_scores(time)

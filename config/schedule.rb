4f3d4fa65abe4e24292afd7477bd3f0b567f353c
Richard Huang <flyerhzm@gmail.com>
2013-05-28 12:56:41 +0800

    load environment before run cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,5 @@
 set :output, "/home/echidna/echidna.transi.st/current/log/whenever.log"
+job_type :rake, "source /home/echidna/.bashrc && cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 
 every '5 * * * *' do
   rake "cache:hourly_trends"

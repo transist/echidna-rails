ed9c195da087f275463d7fdcd96e031dd30250f6
Rainux Luo <rainux@gmail.com>
2013-05-30 00:53:32 +0800

    Update TencentAgent based on name
    openid of the user isn't stable for different oauth app.

--- app/controllers/tencent/agents_controller.rb
+++ app/controllers/tencent/agents_controller.rb
@@ -7,7 +7,7 @@ class Tencent::AgentsController < ApplicationController
   def callback
     begin
       token = TencentAgent.weibo_client.auth_code.get_token(params[:code]).to_hash.symbolize_keys
-      agent = TencentAgent.find_or_initialize_by(openid: token[:openid])
+      agent = TencentAgent.find_or_initialize_by(name: token[:name])
       agent.update_attributes(token)
       render json: {success: true}
     rescue => e
